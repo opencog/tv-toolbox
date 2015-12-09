f934aeef1d70d5a88dee1a6f405e118d1f62f384
Nil Geisweiller <ngeiswei@gmail.com>
2015-12-09 05:27:50 +0200

    Add gnuplot support + optimize like hell
    Using Rational brings a massive speed up. The distribution itself can
    remain in Double without lose of precision.

--- distributional-tv-experiment.hs
+++ distributional-tv-experiment.hs
@@ -1,17 +1,30 @@
 #!/usr/bin/env runhaskell
 
+import Control.Concurrent (threadDelay)
+import Math.Gamma (gamma)
+import Data.Ratio ((%))
 import Data.Number.CReal (CReal)
+import Data.Number.BigFloat (BigFloat, Prec10, Prec50)
 import Math.Combinatorics.Exact.Binomial (choose)
 import System.Environment (getArgs)
 import Text.Format (format)
-import Data.MultiMap (MultiMap, fromList, toList, foldr, map, size)
+import Data.MultiMap (MultiMap, fromList, toList, insert, toMap,
+                      mapKeys, foldr, foldrWithKey, map, size, empty)
+import Data.Map (Map, map, toList)
 import Debug.Trace (trace)
+import Graphics.Gnuplot.Simple (plotPath, Attribute(XRange))
 
-type MyFloat = CReal
+-- type MyFloat = Float
+type MyFloat = Double
+-- type MyFloat = BigFloat Prec10
+-- type MyFloat = BigFloat Prec50
 
 -- Distribution type, maps a first order probability to its second
--- order probability (the probability of this probability).
+-- order probability (or strength to probability).
 type Dist = MultiMap MyFloat MyFloat
+
+-- Like Dist but each strength is unique
+type Hist = Map MyFloat MyFloat
 
 main :: IO ()
 main = do
@@ -32,61 +45,81 @@ main = do
     -- k = read k_str :: Integer
     -- Directly enter them
     sA = 0.5
-    cA = 0.2
+    nA = 50000
     sB = 0.3
-    cB = 0.3
+    nB = 200
     sC = 0.2
-    cC = 0.2
+    nC = 300
     sAB = 0.3
-    cAB = 0.1
+    nAB = 400
     sBC = 0.4
-    cBC = 0.2
-    k = 10
+    nBC = 500
+    k = 1000
+    hres = 100000                  -- number of bins in the histogram
 
   -- Calculate corresponding counts
   let
-    nA = confidenceToCount cA k
     xA = strengthToCount sA nA
-    nB = confidenceToCount cB k
-    xB = strengthToCount sB nA
-    nC = confidenceToCount cC k
-    xC = strengthToCount sC nA
-    nAB = confidenceToCount cAB k
+    xB = strengthToCount sB nB
+    xC = strengthToCount sC nC
     xAB = strengthToCount sAB nAB
-    nBC = confidenceToCount cBC k
     xBC = strengthToCount sBC nBC
-  putStrLn (format "nA = {0}, xA = {1}, nB = {2}, xB = {3}, nC = {4}, xC = {5}, nAB = {6}, xAB = {7}, nBC = {8}, xBC = {9}" (Prelude.map show [nA, xA, nB, xB, nC, xC, nAB, xAB, nBC, xBC]))
+  putStrLn (format "xA = {0}, xB = {1}, xC = {2}, xAB = {3}, xBC = {4}"
+            (Prelude.map show [xA, xB, xC, xAB, xBC]))
 
   -- Generate corresponding distributions
   let
-    dA = genDist sA cA k
-    dB = genDist sB cB k
-    dC = genDist sC cC k
-    dAB = genDist sAB cAB k
-    dBC = genDist sBC cBC k
-  putStrLn (format "dA = {0}\ndB = {1}\ndC = {2}\ndAB = {3}\ndBC = {4}" (Prelude.map (show . toList) [dA, dB, dC, dAB, dBC]))
+    hA = genHist sA nA k
+    hB = genHist sB nB k
+    hC = genHist sC nC k
+    hAB = genHist sAB nAB k
+    hBC = genHist sBC nBC k
 
-  -- Compute the result of deduction
-  let
-    dAC = deduction dA dB dC dAB dBC
-  putStrLn (format "dAC = {0}" [show (toList dAC)])
-  putStrLn (format "dAC total = {0}, size = {1}" [show (distSum dAC),
-                                                  show (Data.MultiMap.size dAC)])
+  putStrLn (format "hA = {0}" [(show . Data.Map.toList) hA])
+  plotPath [XRange (0.0, 1.0)] (toPath hA)
 
-  -- Normalize the distribution
-  let
-    dACnorm = normDist dAC
-  putStrLn (format "dACnorm = {0}" [show (toList dACnorm)])
-  putStrLn (format "dACnorm total = {0}, size = {1}"
-            [show (distSum dACnorm), show (Data.MultiMap.size dACnorm)])
+  putStrLn (format "hB = {0}" [(show . Data.Map.toList) hB])
+  plotPath [XRange (0.0, 1.0)] (toPath hB)
+
+  putStrLn (format "hC = {0}" [(show . Data.Map.toList) hC])
+  plotPath [XRange (0.0, 1.0)] (toPath hC)
+
+  putStrLn (format "hAB = {0}" [(show . Data.Map.toList) hAB])
+  plotPath [XRange (0.0, 1.0)] (toPath hAB)
+
+  putStrLn (format "hBC = {0}" [(show . Data.Map.toList) hBC])
+  plotPath [XRange (0.0, 1.0)] (toPath hBC)
+
+  threadDelay 10000000
+
+  -- putStrLn (format "dA = {0}\ndB = {1}\ndC = {2}\ndAB = {3}\ndBC = {4}" (Prelude.map (show . toList) [dA, dB, dC, dAB, dBC]))
+
+  -- threadDelay 10000000
+  -- -- Compute the result of deduction
+  -- let
+  --   dAC = deduction dA dB dC dAB dBC
+  -- putStrLn (format "dAC = {0}" [show (toList dAC)])
+  -- putStrLn (format "dAC total = {0}, size = {1}" [show (distSum dAC),
+  --                                                 show (Data.MultiMap.size dAC)])
+
+  -- -- Normalize the distribution
+  -- let
+  --   dACnorm = normalize dAC
+  -- putStrLn (format "dACnorm = {0}" [show (toList dACnorm)])
+  -- putStrLn (format "dACnorm total = {0}, size = {1}"
+  --           [show (distSum dACnorm), show (Data.MultiMap.size dACnorm)])
+
+-- Turn a histogram into a plotable path
+toPath h = [(realToFrac s :: Double, realToFrac p :: Double)
+            | (s, p) <- (Data.Map.toList h)]
 
 -- Using the fact that c = n / (n+k) we infer that n = c*k / (1-c)
 confidenceToCount :: MyFloat -> Integer -> Integer
-confidenceToCount c k = round (c*(fromInteger k) / (1 - c))
+confidenceToCount c k = floor (c*(fromInteger k) / (1 - c))
 
 -- x = s*n
 strengthToCount :: MyFloat -> Integer -> Integer
-strengthToCount s n = round (s * (fromInteger n))
+strengthToCount s n = floor (s * (fromInteger n))
 
 -- Return the sum of the probabilities of the distribution. It should
 -- normally be equal to 1.0
@@ -95,25 +128,36 @@ distSum d = Data.MultiMap.foldr (+) 0 d
 
 -- Given a simple TV <s, c> and a lookahead k, generate the
 -- corresponding distribution.
-genDist :: MyFloat -> MyFloat -> Integer -> Dist
-genDist s c k =
-  fromList [(((xreal + (cxreal)) / (nreal + kreal)), p n x k cx)
-           | cx <- [0..k], let cxreal = fromIntegral cx]
-  where
-    n = confidenceToCount c k
-    nreal = fromInteger n
-    x = strengthToCount s n
-    xreal = fromInteger x
-    kreal = fromInteger k
+genDist :: MyFloat -> Integer -> Integer -> Dist
+genDist s n k =
+  fromList [(fromRational ((x+cx) % (n+k)), prob n x k cx) | cx <- [0..k]]
+  where x = strengthToCount s n
+
+-- Like genDist but output a histogram directly
+genHist :: MyFloat -> Integer -> Integer -> Hist
+genHist s n k = toHist (genDist s n k)
 
 -- Normalize a distribution so that it sums up to 1
-normDist :: Dist -> Dist
-normDist d = Data.MultiMap.map (\x -> x / ds) d 
+normalize :: Dist -> Dist
+normalize d = Data.MultiMap.map (\x -> x / ds) d
   where ds = distSum d
 
+-- Return the nearest (lower) bin corresponding to a strength
+bin :: Integer -> MyFloat -> MyFloat
+bin n s = fromRational ((floor (s * (fromInteger n))) % n)
+
+-- Turn a distribution into a histogram (sum up the duplicated
+-- probabilities).
+toHist :: Dist -> Hist
+toHist d = Data.Map.map sum (toMap d)
+
+-- Turn a distribution into a histogram of n bins
+toBinHist :: Dist -> Integer -> Hist
+toBinHist d n = toHist (mapKeys (bin n) d)
+
 -- P(x+X successes in n+k trials | x successes in n trials)
-p :: Integer -> Integer -> Integer -> Integer -> MyFloat
-p n x k cx = (fromIntegral num) / (fromIntegral den)
+prob :: Integer -> Integer -> Integer -> Integer -> MyFloat
+prob n x k cx = fromRational (num % den)
   where num = ((n+1)*(choose k cx)*(choose n x))
         den = ((k+n+1)*(choose (k+n) (cx+x)))
 
@@ -163,10 +207,10 @@ deductionFormula sA sB sC sAB sBC =
 deduction :: Dist-> Dist -> Dist -> Dist -> Dist -> Dist
 deduction dA dB dC dAB dBC =
   fromList [ (deductionFormula sA sB sC sAB sBC, pA * pB * pC * pAB * pBC)  |
-             (sA, pA) <- (toList dA),
-             (sB, pB) <- (toList dB),
-             (sC, pC) <- (toList dC),
-             (sAB, pAB) <- (toList dAB),
-             (sBC, pBC) <- (toList dBC),
+             (sA, pA) <- (Data.MultiMap.toList dA),
+             (sB, pB) <- (Data.MultiMap.toList dB),
+             (sC, pC) <- (Data.MultiMap.toList dC),
+             (sAB, pAB) <- (Data.MultiMap.toList dAB),
+             (sBC, pBC) <- (Data.MultiMap.toList dBC),
              deductionConsistency sA sB sAB,
              deductionConsistency sB sC sBC]
